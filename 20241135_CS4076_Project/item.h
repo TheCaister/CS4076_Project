9477419bdcc3b97a822e3120ec53c77103bc6664
TheCaister <edisoncai2001@gmail.com>
2022-02-20 18:07:57 +0000

    Making a raffle item with multi inheritance

--- 20241135_CS4076_Project/item.h
+++ 20241135_CS4076_Project/item.h
@@ -8,23 +8,40 @@ using namespace std;
 
 class Item {
 private:
-	string description;
-	string longDescription;
-	int weightGrams;
-	float value;
-	bool weaponCheck;
+    string description;
+    string longDescription;
+    int weightGrams;
+    float value;
+    bool weaponCheck;
 
 public:
     Item (string description, int inWeight, float inValue);
     Item (string description);
-	string getShortDescription();
+    string getShortDescription();
     string getLongDescription();
-	int getWeight();
-	void setWeight(int weightGrams);
-	float getValue();
-	void setValue(float value);
-	int getWeaponCheck();
-	void setWeaponCheck(int weaponCheck);
+    int getWeight();
+    void setWeight(int weightGrams);
+    float getValue();
+    void setValue(float value);
+    int getWeaponCheck();
+    void setWeaponCheck(int weaponCheck);
+};
+
+class ConsumableItem : public Item{
+public:
+    void consumeItem();
+};
+
+class LuckItem : public Item{
+private:
+    float successChance;
+  public:
+    bool tryChance();
+};
+
+class Raffle : public ConsumableItem, public LuckItem{
+public:
+    int value;
 };
 
 #endif /*ITEM_H_*/
